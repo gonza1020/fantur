497f7342806006810a9eb35a6773f69a994f23e5
gonza1020 <gonzalozoloaga44@gmail.com>
2022-05-06 17:50:12 -0300

    Initial commit

---
+++ src/main/java/com/agencia/fantur/FanturApplication.java
@@ -0,0 +1,17 @@
+package com.agencia.fantur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class FanturApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FanturApplication.class, args);
+		System.out.println("Ejecutando aplicacion en Spring Boot");
+	}
+
+}
---
+++ src/test/java/com/agencia/fantur/FanturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.fantur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
