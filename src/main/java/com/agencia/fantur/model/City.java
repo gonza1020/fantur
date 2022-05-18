6b3893fb1a1067e6836fa20ca729a2ac639960de
gonza1020 <gonzalozoloaga44@gmail.com>
2022-05-17 21:34:03 -0300

    City relationship

---
+++ src/main/java/com/agencia/fantur/model/City.java
@@ -0,0 +1,19 @@
+package com.agencia.fantur.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "city")
+public class City extends BaseEntity<Long>{
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "zipCode", nullable = false)
+    private int zipCode;
+
+}
---
+++ src/main/java/com/agencia/fantur/repository/CityRepository.java
@@ -0,0 +1,8 @@
+package com.agencia.fantur.repository;
+
+import com.agencia.fantur.model.City;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends BaseRepository<City,Long>{
+}
