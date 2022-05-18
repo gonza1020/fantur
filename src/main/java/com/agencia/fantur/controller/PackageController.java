4ed464954f53b4742f8ea084530a5d28bba5bff7
gonza1020 <gonzalozoloaga44@gmail.com>
2022-05-18 14:27:20 -0300

    Refactor controllers and services

--- src/main/java/com/agencia/fantur/controller/PackageController.java
+++ src/main/java/com/agencia/fantur/controller/PackageController.java
@@ -1,6 +1,8 @@
 package com.agencia.fantur.controller;
 
 import com.agencia.fantur.model.Package;
+import com.agencia.fantur.model.PremiumPackage;
+import com.agencia.fantur.model.StandardPackage;
 import com.agencia.fantur.service.BaseService;
 import com.agencia.fantur.service.PackageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,40 +12,50 @@ import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/packages")
-public class PackageController implements BaseController <Package,Long>{
+public class PackageController{
 
     @Autowired
     private BaseService<Package,Long> packageService;
 
-    @PostMapping()
-    @Override
-    public ResponseEntity<Package> create(@RequestBody Package entity) {
-        return new ResponseEntity<>(packageService.save(entity), HttpStatus.CREATED);
+    @Autowired
+    private BaseService<StandardPackage,Long> standardPackageService;
+
+    @Autowired
+    private BaseService<PremiumPackage,Long> premiumPackageService;
+
+    @PostMapping("/standard")
+    public ResponseEntity<StandardPackage> create(@RequestBody StandardPackage entity) {
+
+            return new ResponseEntity<>(standardPackageService.save(entity), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/premium")
+    public ResponseEntity<PremiumPackage> create(@RequestBody PremiumPackage entity) {
+        return new ResponseEntity<>(premiumPackageService.save(entity), HttpStatus.CREATED);
     }
 
 
 
     @GetMapping()
-    @Override
     public ResponseEntity<List<Package>> getAll() {
         return new ResponseEntity<>(packageService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    @Override
     public ResponseEntity<Package> getById(@PathVariable Long id) {
         return new ResponseEntity<>(packageService.findById(id),HttpStatus.OK);
     }
 
-    @Override
-    public ResponseEntity<Package> update(Package entity, Long aLong) {
-        return null;
+    @PutMapping("{id}")
+    public ResponseEntity<Package> update(Package entity, Long id) {
+        return new ResponseEntity<>(packageService.update(entity,id),HttpStatus.ACCEPTED);
     }
+
     @DeleteMapping("{id}")
-    @Override
     public void delete(@PathVariable Long id) {
         packageService.delete(id);
     }
---
+++ src/main/java/com/agencia/fantur/service/StandarPackageServiceImpl.java
@@ -0,0 +1,8 @@
+package com.agencia.fantur.service;
+
+import com.agencia.fantur.model.StandardPackage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StandarPackageServiceImpl extends BaseServiceImpl<StandardPackage,Long>{
+}
