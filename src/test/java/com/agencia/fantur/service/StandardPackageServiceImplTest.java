1dbae315a21ce088124e550313c013012cb20529
gonza1020 <gonzalozoloaga44@gmail.com>
2022-05-21 16:39:28 -0300

    Test package price

---
+++ src/test/java/com/agencia/fantur/service/StandardPackageServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.agencia.fantur.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StandardPackageServiceImplTest {
+
+    @Test
+    void calculatePrice() {
+    }
+}
